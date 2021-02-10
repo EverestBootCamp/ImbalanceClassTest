0e9a6fdb1801dae6508e099258feb23f83076c0c
ramojjalamanasa <31012435+ramojjalamanasa@users.noreply.github.com>
2021-02-09 19:28:06 -0500

    Delete my_module.py

--- src/tests/my_module.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def mean(df, column):
-    return df.groupby("class")[column].mean().to_dict()
-
-def pipeline(column):
-    data = pd.read_csv("./data_transformed.csv")
-    data = aggregate_mean(data, column)
-    return data
