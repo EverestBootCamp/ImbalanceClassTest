d7ac8c8ae4f186c347cb8f373559bc3c143a6341
Dilip <dilipsahoo823@gmail.com>
2021-02-24 00:26:19 -0500

    updated output file

--- output/wining.py
+++ output/wining.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
 from sklearn.tree import DecisionTreeClassifier
-from tpot.builtins import ZeroCount
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.8855242114144655
+# Average CV score on the training set was: 0.8927949688898099
 exported_pipeline = make_pipeline(
-    ZeroCount(),
-    DecisionTreeClassifier()
+    StackingEstimator(estimator=DecisionTreeClassifier(criterion="entropy", max_depth=2, min_samples_leaf=11, min_samples_split=15)),
+    ExtraTreesClassifier(bootstrap=True, criterion="gini", max_features=0.2, min_samples_leaf=1, min_samples_split=6, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
