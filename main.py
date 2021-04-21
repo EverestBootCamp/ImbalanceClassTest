892895ea099ed3c03080dc4b151d1ddefe0a23a0
“Mohammad.Hassanpour” <Mohammad.hassanpour71@gmail.com>
2021-04-21 19:38:28 -0400

    updating main.py

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ from sklearn.metrics import f1_score
 from hyperopt import hp, fmin, tpe, Trials, STATUS_OK
 from hyperopt import space_eval
 from HyperOpt_code.Hyperoptsvm import controller
+from XGBoost.XGboostnew import XGBModel
 from SVM_trainclass.T103 import Model
 from T102_Annotations.data_annotation import Annotation
+from sklearn.metrics import log_loss
+import xgboost as xgb
 
 
 
@@ -26,7 +29,7 @@ def arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 
-    parser.add_argument("--algorithm", default="svm", help="algorithm for modelling", type=str, choices=['svm'])
+    parser.add_argument("--algorithm", default="xgb", help="algorithm for modelling", type=str, choices=['svm','xgb'])
     parser.add_argument("--percentage", default=0.1, help="percentage of data used for training", type=float)
 
 
@@ -35,22 +38,41 @@ def arguments():
 
 
 def main():
+    logging.basicConfig(filename='logfile.log', filemode="w", format ='%(asctime)s %(message)s' ,level=logging.INFO)
+    percentage = args.percentage
 
-    if(args.algorithm == 'svm'):
+    logging.info('*******Creating test and train data sets*******')
+    train_df_Class = Annotation(test_size=percentage,output_dataframe=str(percentage))
+    train_df = train_df_Class.dataframe()
+    test_df_Class = Annotation(test_size=percentage,output_dataframe='test')
+    test_df = test_df_Class.dataframe()
 
-        logging.basicConfig(filename='logfile.log', filemode="w", format ='%(asctime)s %(message)s' ,level=logging.INFO)
-        percentage = args.percentage
+    if (args.algorithm == 'svm'):
 
-        logging.info('*******Creating test and train data sets*******')
-        train_df_Class = Annotation(test_size=percentage,output_dataframe=str(percentage))
-        train_df = train_df_Class.dataframe()
-        test_df_Class = Annotation(test_size=percentage,output_dataframe='test')
-        test_df = test_df_Class.dataframe()
+        
         logging.info('*******Creating the SVM model*******')
         svm_model = Model(train=train_df,test=test_df)
         logging.info('*******Optimizing the Hyper Parameters*******')
         hyperOpt_class = controller(svm_model)
         hyperOpt_class.optimize_hyperparam()
+        logging.info('*******HyperOptimization Finished*******')
+
+    elif (args.algorithm == 'xgb'):
+        logging.info('*******Initializing the XGB model*******')
+        xgb_model = XGBModel(train=train_df,test=test_df)
+        logging.info('*******Optimizing the Hyper Parameters*******')
+        xgb_model.optimize_hyperparam()
+        logging.info('*******HyperOptimization Finished*******')
+
+
+
+
+
+
+
+
+
+
     
 
 
