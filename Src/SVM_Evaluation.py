918f40030bac759abd7c8b29b2f621939d664b5c
Keerthi Prakash <71408369+Keerthic4@users.noreply.github.com>
2021-05-03 17:36:30 -0400

    Adds python script

---
+++ Src/SVM_Evaluation.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+This script prompts a user to test SVM model.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import plot_precision_recall_curve
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import plot_roc_curve
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from Hyperoptsvm import controller
+from SvmModel import Model
+
+
+model = Model("data_transformed.csv",
+              "data_transformed.csv")
+
+
+hyperCl = controller(model)
+hyperparams = hyperCl.optimize_hyperparam()
+
+
+# print(hyperparams)
+
+# import data
+data_transformed = pd.read_csv('data_transformed.csv')
+
+# avoid this ugly slicing by using a two-dim dataset
+X = data_transformed.iloc[:5000, :-1]
+y = data_transformed.iloc[:5000, :]['Class']
+
+
+# using 75% of the data for training and 30% for testing (with
+# stratification for imbalanced class)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.30, stratify=y, random_state=123)
+
+# Standardize features by removing the mean and scaling to unit variance
+sc = StandardScaler()
+scaled_X_train = sc.fit_transform(X_train)
+scaled_X_test = sc.transform(X_test)
+
+
+class Test:
+    """
+    A class to represent trains the model on best paramters and
+    evaluate SVM model
+
+    """
+
+    def __init__(self, hyperparams, scaled_X_train, scaled_X_test, y_train):
+        self.clf = svm.SVC(
+            **hyperparams,
+            class_weight='balanced',
+            random_state=123,
+            probability=True)
+        self.clf.fit(scaled_X_train, y_train)
+        self.pred = self.clf.predict(scaled_X_test)
+
+    def score(self, y_test):
+        # Compute confusion matrix, roc_auc_score, Cohen_kappa_score
+
+        performance = {"confusion_matrix": confusion_matrix(y_test, self.pred),
+                       "roc_auc_score": roc_auc_score(y_test, self.pred),
+                       "cohen_score": cohen_kappa_score(y_test, self.pred)}
+        return performance
+
+    def roc(self):
+        # ROC Curve
+        clf = self.clf
+        svc_disp = plot_roc_curve(clf, scaled_X_test, y_test)
+        svc_disp.ax_.set_xlabel('False Positive Rate')
+        svc_disp.ax_.set_ylabel('True Positive Rate')
+        svc_disp.ax_.set_title('ROC curve')
+        plt.show()
+        plt.savefig('roc.png')
+
+    def prcurve(self):
+        #Precision Recall Curve
+        clf = self.clf
+        y_score = clf.decision_function(scaled_X_test)
+        average_precision = average_precision_score(y_test, y_score)
+        disp = plot_precision_recall_curve(clf, scaled_X_test, y_test)
+        disp.ax_.set_xlabel('Recall')
+        disp.ax_.set_ylabel('Precision')
+        disp.ax_.set_title('2-class Precision-Recall curve: '
+                           'AP={0:0.2f}'.format(average_precision))
+        plt.show()
+        plt.savefig('prcurve.png')
